6c035ea6c6c2e8f3d4e7c8ff86d7450c0d909d06
SirAlyon <alessiosada04@gmail.com>
2022-06-16 16:04:47 +0200

    initial commit - add structure

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<table style="width:100%">
+    <tr>
+      <th>TITLE</th>
+      <th>DESCRIPTION</th>
+      <th>THUMB</th>
+      <th>PRICE</th>
+      <th>SERIES</th>
+      <th>SALE_DATA</th>
+      <th>TYPE</th>
+    </tr>
+    <tr>
+
+    </tr>
+    <tr>
+
+    </tr>
+  </table>
+@endsection
